a2718ea39d8f871bb990a52ffe8bb8d53603a9f1
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2019-08-21 22:46:11 -0500

    Calculate budget

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,29 +1,180 @@
 // IIFE allows to have data privacy because it creates a new execution scope
-var budgetController = (function() {
-    var x = 23;
+var budgetController = (function () {
 
-    var add = function(a) {
-        return x + a;
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var budget = {
+        allItems: {
+            expenses: [],
+            incomes: []
+        },
+        totals: {
+            expenses: 0,
+            incomes: 0,
+        },
+        budget: 0,
+        percentage: -1
     }
+    var calculateTotal = function (type) {
+        var sum = 0;
+        budget.allItems[type].forEach(function (current) {
+            sum += current.value;
+        });
+        budget.totals[type] = sum;
+    };
 
-    // Public API
+    /* budget.prototype.getBudget = function () {
+        this.budget = this.totals['incomes'] - this.totals['expenses'];
+        return this.budget;
+    };
+    budget.prototype.getPercentageSpent = function () {
+        this.percentage = Math.round((this.totals.expenses / this.totals.incomes) * 100);
+        return this.percentage;
+    }; */
+
     return {
-        publicTest: function(b) {
-            return add(b);
+        addItem: function (type, description, value) {
+            var newItem, id;
+
+            // Create a new id
+            if (budget.allItems[type].length > 0) {
+                id = budget.allItems[type][budget.allItems[type].length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+
+            // Create a new item based on inc or exp
+            if (type === 'incomes') {
+                newItem = new Income(id, description, value);
+            } else if (type === 'expenses') {
+                newItem = new Expense(id, description, value);
+            }
+            budget.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        calculateBudget: function () {
+            calculateTotal('expenses');
+            calculateTotal('incomes');
+
+            budget.budget = budget.totals['incomes'] - budget.totals['expenses']
+            if (budget.totals.incomes > 0) {
+                budget.percentage = Math.round((budget.totals.expenses / budget.totals.incomes) * 100);
+            }
+        },
+
+        getBudget: function () {
+            return {
+                budget: budget.budget,
+                totalInc: budget.totals.incomes,
+                totalExp: budget.totals.expenses,
+                percentage: budget.percentage
+            };
+        },
+
+        getAllItems: function () {
+            return budget;
         }
     }
 })();
 
-var uiController = (function() {
+var uiController = (function () {
 
-})();
-
-var appController = (function(budgetController, uiController) {
-    var z = budgetController.publicTest(5);
+    var domStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        expensesList: '.expenses__list',
+        incomesList: '.income__list'
+    };
 
     return {
-        anotherPublicMethod() {
-            console.log(z)
+        getDomStrings: function () {
+            return domStrings;
+        },
+
+        getInput: function () {
+            return {
+                type: document.querySelector(domStrings.inputType).value,
+                description: document.querySelector(domStrings.inputDescription).value,
+                value: parseFloat(document.querySelector(domStrings.inputValue).value)
+            }
+        },
+
+        addListItem: function (obj, type) {
+            var itemHtml, newHtml, element;
+            if (type === 'incomes') {
+                element = domStrings.incomesList;
+                itemHtml = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div ><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
+            } else if (type === 'expenses') {
+                element = domStrings.expensesList;
+                itemHtml = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div ><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div >';
+            }
+
+            newHtml = itemHtml.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
+        },
+
+        clearFields: function () {
+            var fields = document.querySelectorAll(domStrings.inputDescription + ', ' + domStrings.inputValue);
+            var fieldsArray = Array.prototype.slice.call(fields);
+            fieldsArray.forEach(function (current, index, array) {
+                current.value = "";
+            });
+            fieldsArray[0].focus();
+        }
+    }
+})();
+
+var appController = (function (budgetController, uiController) {
+
+    var setUpListeners = function () {
+        console.log('Setting up listeners...');
+        var domStrings = uiController.getDomStrings();
+
+        document.querySelector(domStrings.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
+        var userInput = uiController.getInput();
+        console.log(userInput);
+
+        if (userInput.description !== "" && !isNaN(userInput.value) && userInput.value > 0) {
+            var newItem = budgetController.addItem(userInput.type, userInput.description, userInput.value);
+            uiController.addListItem(newItem, userInput.type);
+            uiController.clearFields();
+            updateBudget();
+        }
+    };
+
+    var updateBudget = function () {
+        budgetController.calculateBudget();
+        var budget = budgetController.getBudget();
+        console.log(budget);
+    };
+
+    return {
+        init: function () {
+            console.log('Budgety is starting...');
+            setUpListeners();
+            console.log('Budgety has started!');
         }
     }
 })(budgetController, uiController);
+appController.init();
